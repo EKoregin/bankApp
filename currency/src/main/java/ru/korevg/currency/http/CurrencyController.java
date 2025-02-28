2b9b2a4bc93232e236bd108fe6ff0c45001bf8f6
koreginev <evgkoregin@gmail.com>
2025-02-28 13:05:06 +0300

    Добавление swagger

--- currency/src/main/java/ru/korevg/currency/http/CurrencyController.java
+++ currency/src/main/java/ru/korevg/currency/http/CurrencyController.java
@@ -1,7 +1,11 @@
 package ru.korevg.currency.http;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.korevg.currency.service.CbrService;
@@ -9,15 +13,20 @@ import ru.korevg.currency.service.CbrService;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Tag(name = "Валюты")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/currency/v1")
 public class CurrencyController {
 
     private final CbrService cbrService;
 
     @GetMapping("/getCurrency")
+    @Operation(description = "Получение курса валюты по коду на требуемую дату")
     public BigDecimal getCurrency(
+            @Parameter(description = "код валюты (USD)", required = true)
             @RequestParam("code") String code,
+            @Parameter(description = "курс на дату")
             @RequestParam(required = false) LocalDate onDate) {
         return cbrService.requestByCurrencyCodeAndDate(onDate, code.toUpperCase());
     }
