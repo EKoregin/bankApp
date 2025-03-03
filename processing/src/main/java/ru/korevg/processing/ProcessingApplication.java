7cf9ffa21923409d0cf431c096ce2b0d697f7e26
koreginev <evgkoregin@gmail.com>
2025-03-03 13:51:04 +0300

    Add register service

--- processing/src/main/java/ru/korevg/processing/ProcessingApplication.java
+++ processing/src/main/java/ru/korevg/processing/ProcessingApplication.java
@@ -2,8 +2,10 @@ package ru.korevg.processing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients("ru.korevg.processing.client")
 public class ProcessingApplication {
